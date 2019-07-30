eaf07d8e5a91b95c66c6b4b8cef2200401571233
Emil Petersen <emoyly@gmail.com>
2019-07-31 01:00:08 +0200

    Added gang member display

--- player.php
+++ player.php
@@ -30,6 +30,33 @@ Layout::i()->nav();
 <div class="text-center">
     <h1 class="display-4">Welcome <?=User::i()->getName()?></h1>
     <p class="lead"><?=User::i()->getGangID() ? 'Gang: '.$Gang['Name'] : 'You are not in a gang' ?></p>
+    <h2>Gang members</h2>
+    <table class="table container">
+        <thead>
+            <tr>
+                <th>SteamID</th>
+                <th>Rank</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                if(isset($Gang, $Gang['Members'])){
+                    foreach($Gang['Members'] as $Key => $Item){
+                        echo '<tr>';
+                        echo '<td>'.$Item['SteamID'].'</td>';
+                        echo '<td>'.$Item['Rank'].'</td>';
+                        echo '</tr>';
+                    }
+                }
+                else{
+                    echo '<tr>';
+                    echo '<td>No gang members</td>';
+                    echo '<td></td>';
+                    echo '</tr>';
+                }
+            ?>
+        </tbody>
+    </table>
 </div>
 
 <?php
